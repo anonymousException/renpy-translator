04ecf927ba46ebb491263aa9ebe68cb8cc1e960c
anonymousException <lylsixonezero@gmail.com>
2024-01-26 11:11:32 +0800

    <feature>  redirect stderr to gui log

--- src/my_log.py
+++ src/my_log.py
@@ -7,7 +7,8 @@ from datetime import datetime
 timestamp = str(int(time.time()))
 log_path = 'log'+'.txt'
 f=open(log_path,'a+',encoding='utf-8')
-sys.stdout=f
+sys.stdout = f
+sys.stderr = f
 log_lock = threading.Lock()
 def log_print(*objects, sep=' ', end='\n', file=sys.stdout, flush=True):
     log_lock.acquire()
