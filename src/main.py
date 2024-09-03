034f444062100f72cbe0c4d81c53971bc40871ae
anonymousException <lylsixonezero@gmail.com>
2024-09-03 10:55:03 +0800

    <feature> upgrade requirements dependency
    update version to v2.5.1

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ targetDic = dict()
 sourceDic = dict()
 translator = QTranslator()
 
-VERSION = '2.5.0'
+VERSION = '2.5.1'
 
 class MyProxyForm(QDialog, Ui_ProxyDialog):
     def __init__(self, parent=None):
